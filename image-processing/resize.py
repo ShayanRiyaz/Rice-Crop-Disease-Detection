78f31afc6b1429019d3527e4b18b5fe11695028a
Shayan Riyaz <shayanariyaz@gmail.com>
2020-08-15 13:22:04 -0700

    Update resize.py

--- image-processing/resize.py
+++ image-processing/resize.py
@@ -18,6 +18,49 @@ def resize(input_path,folder,column_name):
             imResize = im.resize((150,150), Image.ANTIALIAS)
             #print('CHECK 3')
             imResize.save(f + '.jpg', 'JPEG', quality=90)
+            
+if '__main__' == __name__:
+    input_path = '../input/RiceLeafs/train/Healthy'
+    folder = 'train'
+    column_name = 'Healthy'
+    resize(input_path,folder,column_name)
 
+    input_path = '../input/RiceLeafs/train/BrownSpot'
+    folder = 'train'
+    column_name = 'BrownSpot'
+    resize(input_path,folder,column_name)
 
+    input_path = '../input/RiceLeafs/train/Hispa'
+    folder = 'train'
+    column_name = 'Hispa'
+    resize(input_path,folder,column_name)
 
+    input_path = '../input/RiceLeafs/train/LeafBlast'
+    folder = 'train'
+    column_name = 'LeafBlast'
+    resize(input_path,folder,column_name)
+
+    print('Done with train resizing')
+
+     ## VALIDATION
+    input_path = '../input/RiceLeafs/validation/Healthy'
+    folder = 'validation'
+    column_name = 'Healthy'
+    resize(input_path,folder,column_name)
+
+    input_path = '../input/RiceLeafs/validation/BrownSpot'
+    folder = 'validation'
+    column_name = 'BrownSpot'
+    resize(input_path,folder,column_name)
+
+    input_path = '../input/RiceLeafs/validation/Hispa'
+    folder = 'validation'
+    column_name = 'Hispa'
+    resize(input_path,folder,column_name)
+
+    input_path = '../input/RiceLeafs/validation/LeafBlast'
+    folder = 'validation'
+    column_name = 'LeafBlast'
+    resize(input_path,folder,column_name)
+
+    print('Done with Validation resizing')
